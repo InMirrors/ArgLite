6eb3e4a124b9fef614cd7b05e119135a3f31ede5
InMirrors <50800319+InMirrors@users.noreply.github.com>
2025-12-06 10:53:38 +0800

    refactor: Optimize Formatter class for performance and correctness
    This commit introduces several optimizations to the Formatter class to improve
    performance and ensure correct usage of output streams.
    
    Key changes:
    - Static methods in `Formatter` now accept `const std::ostream&` for the output
      stream parameter, indicating that the stream is not modified.
    - Added `result.reserve(str.size());` in `Formatter::format` to pre-allocate
      memory for the result string, reducing reallocations and improving performance
      when constructing formatted strings.

--- include/ArgLite/Formatter.hpp
+++ include/ArgLite/Formatter.hpp
@@ -20,15 +20,15 @@ namespace ArgLite {
 
 class Formatter {
 public:
-    static std::string red(std::string_view str, std::ostream &os = std::cerr) {
+    static std::string red(std::string_view str, const std::ostream &os = std::cerr) {
         return format(str, ANSI_RED, os);
     }
 
-    static std::string bold(std::string_view str, std::ostream &os = std::cout) {
+    static std::string bold(std::string_view str, const std::ostream &os = std::cout) {
         return format(str, ANSI_BOLD, os);
     }
 
-    static std::string boldUnderline(std::string_view str, std::ostream &os = std::cout) {
+    static std::string boldUnderline(std::string_view str, const std::ostream &os = std::cout) {
         return format(str, ANSI_BOLD_UNDERLINE, os);
     }
 
@@ -38,8 +38,9 @@ private:
     static constexpr std::string_view ANSI_BOLD           = "\x1b[1m";
     static constexpr std::string_view ANSI_BOLD_UNDERLINE = "\x1b[1m\x1b[4m";
 
-    static std::string format(std::string_view str, std::string_view code, std::ostream &os = std::cout) {
+    static std::string format(std::string_view str, std::string_view code, const std::ostream &os = std::cout) {
         std::string result;
+        result.reserve(str.size());
         if (shouldFormat(os)) {
             result.append(code).append(str).append(ANSI_RESET);
         } else {
